217a04f698b4b75f12b9f081168bdf87cf19211b
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2023-03-16 23:26:07 +0900

    kitchen 24: mock test code

---
+++ kotlinPractice/src/test/kotlin/com/example/kotlinPractice/service/mock/IngredientController.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinPractice.service.mock
+
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class IngredientController {
+}
---
+++ kotlinPractice/src/test/kotlin/com/example/kotlinPractice/service/mock/RefrigeratorController.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinPractice.service.mock
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefrigeratorController {
+}
